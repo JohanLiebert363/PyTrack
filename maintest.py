0107c0f4f35849f58907ba14e3877871bd474cfb
Duo <edintahiri.2013@gmail.com>
2026-01-01 14:48:04 +0100

    Add files via upload

---
+++ maintest.py
@@ -0,0 +1,46 @@
+import cv2
+import pyttsx3
+
+
+engine = pyttsx3.init()
+
+
+cap = cv2.VideoCapture(0)
+print("Camera opened:", cap.isOpened())
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Resize for speed
+    frame = cv2.resize(frame, (640, 480))
+
+    boxes, _ = hog.detectMultiScale(
+        frame,
+        winStride=(8, 8),
+        padding=(8, 8),
+        scale=1.05
+    )
+
+    for (x, y, w, h) in boxes:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, "Human", (x, y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+
+    cv2.imshow("Human Detection", frame)
+
+    if len(boxes) > 0:
+        print("Alert: Human detected!")
+        engine.say("Human detected")
+        engine.runAndWait()
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
